d410202fef8cec09d716c18e2827fe345f59f2da
nagyb3 <nagybence2003@gmail.com>
2023-09-21 22:19:11 +0200

    show todo items and submit new one

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -15,6 +15,7 @@ type TodoItemType = {
   userid: string;
   text: string;
   is_done: boolean;
+  _id: string;
 };
 
 export default function Todo({
@@ -22,9 +23,7 @@ export default function Todo({
   setSelectedTool,
   isLoggedIn,
 }: TodoProps) {
-  const [todoItemsList, setTodoItemsList] = useState<
-    TodoItemType[] | undefined | null
-  >(undefined);
+  const [todoItemsList, setTodoItemsList] = useState<TodoItemType[] | null>([]);
 
   const [newTodoState, setNewTodoState] = useState<string>("");
 
@@ -38,7 +37,7 @@ export default function Todo({
           // Authorization: "Bearer " + localStorage.getItem("token"),
         },
         body: JSON.stringify({
-          user_id: localStorage.getItem("username"),
+          username: localStorage.getItem("username"),
         }),
       })
         .then((response) => {
@@ -53,9 +52,8 @@ export default function Todo({
     }
   }, []);
 
-  console.log(todoItemsList);
-
-  function handleSubmitTodoItem() {
+  function handleSubmitTodoItem(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
     fetch(`${import.meta.env.VITE_API_URL}/todo`, {
       method: "POST",
       headers: {
@@ -63,11 +61,14 @@ export default function Todo({
         // Authorization: "Bearer " + localStorage.getItem("token"),
       },
       body: JSON.stringify({
-        user_id: localStorage.getItem("username"),
+        username: localStorage.getItem("username"),
         text: newTodoState,
       }),
     })
       .then((response) => {
+        if (response.ok) {
+          window.location.reload();
+        }
         return response.json();
       })
       .then((data) => {
@@ -104,6 +105,8 @@ export default function Todo({
       });
   }
 
+  console.log(todoItemsList);
+
   return (
     <div className="min-h-[calc(100vh-70px)]">
       <StudySectionsNavbar
@@ -111,14 +114,19 @@ export default function Todo({
         setSelectedTool={setSelectedTool}
       />
       <h1 className="m-16 text-center text-2xl">Your current todo list: </h1>
-      <form action="">
+      <form
+        onSubmit={(e) => handleSubmitTodoItem(e)}
+        className="flex flex-col items-center gap-4"
+      >
         <label htmlFor="new-todo">New element:</label>
         <input
           className="border-input bg-background ring-offset-background placeholder:text-muted-foreground focus-visible:ring-ring flex
-            h-10 w-full rounded border-[1px] border-black p-1 px-3 py-2 text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
+            h-10 rounded border-[1px] border-black p-1 px-3 py-2 text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
           type="text"
           id="new-todo"
           placeholder="Element's name.."
+          onChange={(e) => setNewTodoState(e.target.value)}
+          value={newTodoState}
         />
         <input
           type="submit"
@@ -126,13 +134,33 @@ export default function Todo({
           className="text-kg rounded border-[1px] border-black bg-blue-700 p-2 text-white"
         />
       </form>
-      {todoItemsList !== undefined && todoItemsList !== null ? (
-        <ul>
-          {todoItemsList.map((todo) => {
-            return <li>{todo.text}</li>;
-          })}
-        </ul>
-      ) : undefined}
+      <div className="flex flex-col items-center">
+        <h1 className="m-8 text-lg">Your todo items:</h1>
+        {todoItemsList !== null &&
+        todoItemsList.length !== 0 &&
+        todoItemsList !== null ? (
+          <ul className="flex flex-col gap-4">
+            {todoItemsList.map((todo) => {
+              return (
+                <li key={todo._id}>
+                  <div className="flex justify-between gap-8 rounded border-2 border-black bg-gray-200 p-3">
+                    <p className={todo.is_done ? "line-through" : undefined}>
+                      {todo.text}
+                    </p>
+                    <div
+                      className={`h-[30px] w-[30px] rounded-full ${
+                        todo.is_done ? "bg-green-500" : "bg-red-500"
+                      } }`}
+                    ></div>
+                  </div>
+                </li>
+              );
+            })}
+          </ul>
+        ) : (
+          <p>You don&apos;t have any todo items yet...</p>
+        )}
+      </div>
     </div>
   );
 }
